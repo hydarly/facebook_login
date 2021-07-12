16b2868117d7848b071330470a2e124a3a4de2c5
hydarly <hydar.1994.jbyle@gmail.com>
2021-07-12 19:12:00 +0300

    first

---
+++ index.py
@@ -0,0 +1,76 @@
+import os
+
+import flask
+import requests_oauthlib
+from requests_oauthlib.compliance_fixes import facebook_compliance_fix
+
+# Your ngrok url, obtained after running "ngrok http 5000"
+URL = "https://d7f12cfa8fcd.ngrok.io"
+
+FB_CLIENT_ID = "335582323665126"
+FB_CLIENT_SECRET = "cc037fefcf9feef35a015f1767345f21"
+
+FB_AUTHORIZATION_BASE_URL = "https://www.facebook.com/dialog/oauth"
+FB_TOKEN_URL = "https://graph.facebook.com/oauth/access_token"
+
+FB_SCOPE = ["email"]
+
+# This allows us to use a plain HTTP callback
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return """
+    <a href="/fb-login">Login with Facebook</a>
+    """
+
+
+@app.route("/fb-login")
+def login():
+    facebook = requests_oauthlib.OAuth2Session(
+        FB_CLIENT_ID, redirect_uri=URL + "/fb-callback", scope=FB_SCOPE
+    )
+    authorization_url, _ = facebook.authorization_url(FB_AUTHORIZATION_BASE_URL)
+
+    return flask.redirect(authorization_url)
+
+
+@app.route("/fb-callback")
+def callback():
+    facebook = requests_oauthlib.OAuth2Session(
+        FB_CLIENT_ID, scope=FB_SCOPE, redirect_uri=URL + "/fb-callback"
+    )
+
+    # we need to apply a fix for Facebook here
+    facebook = facebook_compliance_fix(facebook)
+
+    facebook.fetch_token(
+        FB_TOKEN_URL,
+        client_secret=FB_CLIENT_SECRET,
+        authorization_response=flask.request.url,
+    )
+
+    # Fetch a protected resource, i.e. user profile, via Graph API
+
+    facebook_user_data = facebook.get(
+        "https://graph.facebook.com/me?fields=id,name,email,picture{url}"
+    ).json()
+
+    email = facebook_user_data["email"]
+    name = facebook_user_data["name"]
+    picture_url = facebook_user_data.get("picture", {}).get("data", {}).get("url")
+
+    return f"""
+    User information: <br>
+    Name: {name} <br>
+    Email: {email} <br>
+    Avatar <img src="{picture_url}"> <br>
+    <a href="/">Home</a>
+    """
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
